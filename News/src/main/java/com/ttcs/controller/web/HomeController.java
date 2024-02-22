0c5fbbd8470e1da2e4e70eeee8ceac251200dd5f
Uchiha-Ichi <102291112+Uchiha-Ichi@users.noreply.github.com>
2024-02-22 13:47:53 +0700

    demo home

--- News/src/main/java/com/ttcs/controller/web/HomeController.java
+++ News/src/main/java/com/ttcs/controller/web/HomeController.java
@@ -9,12 +9,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ttcs.model.UserModel;
+
 
 @WebServlet(urlPatterns = {"/trang-chu"})
 public class HomeController extends HttpServlet{
 	private static final long serialVersionUID = 2686801510274002166L;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-         RequestDispatcher rd = request.getRequestDispatcher("/views/web/home.jsp");
+		UserModel  userModel = new UserModel();
+		userModel.setFullName("Hello");
+		request.setAttribute("model", userModel);
+         RequestDispatcher rd = request.getRequestDispatcher("./decorators/web.jsp");
          rd.forward(request, response);
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
